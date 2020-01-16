ceec226543158b0099c9e5630bab78eac29c5bc3
Ruby Jasmin <jasmin.ruby@gmail.com>
2020-01-16 00:25:02 -0500

    refactor(core-api): :sparkles: change import syntax for core library

--- apps/core-api/src/showcases/showcasesController.ts
+++ apps/core-api/src/showcases/showcasesController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { scrapeShowcases, scrapeRosterData } from "@goscout/core";
+import gs from "@goscout/core";
 import { handleError } from "../utils";
 
 export async function sync(req: Request, res: Response): Promise<Response> {
@@ -8,10 +8,10 @@ export async function sync(req: Request, res: Response): Promise<Response> {
   const upcomingUrl = `${host}/showcases`;
 
   try {
-    const showcases = await scrapeShowcases([ pastUrl, upcomingUrl ]);
+    const showcases = await gs.scrapeShowcases([pastUrl, upcomingUrl]);
 
     for (const showcase of showcases) {
-      const roster = await scrapeRosterData(showcase.url);
+      const roster = await gs.scrapeRosterData(showcase.url);
       showcase.roster = roster;
     }
 
