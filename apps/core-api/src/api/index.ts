3756c07af0ebdfc3c40be0f76d58a6aa12cbea1e
Ruby Jasmin <jasmin.ruby@gmail.com>
2020-01-16 00:08:18 -0500

    feat(core-api): :sparkles: add users routes to api router

--- apps/core-api/src/api/index.ts
+++ apps/core-api/src/api/index.ts
@@ -1,6 +1,10 @@
 import { Application, Router } from "express";
+import { usersRoutes } from "../users/usersRoutes";
 
 const use = (app: Application, router: Router): void => {
+  router.get("/warmup", (req, res) => res.send({ status: "ok" }));
+  usersRoutes(router);
+
   app.use("/api/v1", router);
 };
 
