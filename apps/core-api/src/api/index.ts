5eaa8c19306705bee8ad6183b72216c59cc9a991
Ruby Jasmin <jasmin.ruby@gmail.com>
2020-01-16 00:17:41 -0500

    feat(core-api): :sparkles: add showcases routes to api

--- apps/core-api/src/api/index.ts
+++ apps/core-api/src/api/index.ts
@@ -1,9 +1,11 @@
 import { Application, Router } from "express";
 import { usersRoutes } from "../users/usersRoutes";
+import { showcasesRoutes } from "../showcases/showcasesRoutes";
 
 const use = (app: Application, router: Router): void => {
   router.get("/warmup", (req, res) => res.send({ status: "ok" }));
   usersRoutes(router);
+  showcasesRoutes(router);
 
   app.use("/api/v1", router);
 };
